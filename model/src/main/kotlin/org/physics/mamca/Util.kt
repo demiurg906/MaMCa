58b7914f4125ba856e3262cdc5b6111df2a72393
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-11-14 06:35:46 +0300

    ENH: some utils added

---
+++ model/src/main/kotlin/org/physics/mamca/Util.kt
@@ -0,0 +1,31 @@
+package org.physics.mamca
+
+import java.io.File
+import java.lang.Math.acos
+import java.nio.file.Files
+import java.util.*
+
+
+val random = Random()
+
+fun randomTheta(): Double = acos(2 * random.nextDouble() - 1)
+fun randomPhi(): Double = PI2 * random.nextDouble()
+
+fun equalsDouble(one: Double, another: Double): Boolean {
+    return Math.abs(one - another) < DELTA
+}
+
+fun <T>pairs(list: List<T>): List<Pair<T, T>> {
+    /**
+     * return список всевозможных различных пар из элементов списка list
+     */
+    val res: MutableList<Pair<T, T>> = mutableListOf()
+    for (i in 0 until list.size) {
+        for (j in (i + 1) until list.size) {
+            res.add(Pair(list[i], list[j]))
+        }
+    }
+    return res
+}
+
+fun deleteFile(path: String) = Files.delete(File(path).toPath())
