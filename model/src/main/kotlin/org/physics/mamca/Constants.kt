29cc7fa8d84fd46ba16a3361047c751ce9c8d691
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-11-14 06:33:53 +0300

    ENH: add constats

---
+++ model/src/main/kotlin/org/physics/mamca/Constants.kt
@@ -0,0 +1,11 @@
+package org.physics.mamca
+
+const val PI = Math.PI
+const val PI2 = 2 * PI
+
+// точность сравнение чисел с плавающей запятой
+const val DELTA = 1e-6
+
+// расстояния, на которых заметно обменное и дипольное взаимодействия соответственно
+const val EX_DIST = 1
+const val DIPOL_DIST = 30
