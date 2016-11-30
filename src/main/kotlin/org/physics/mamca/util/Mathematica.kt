558c38d6e2bf48cd24cfb26dd44e61e945d0b02b
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-11-30 10:54:07 +0300

    MAINT: little fix in command to solve equation

--- src/main/kotlin/org/physics/mamca/util/Mathematica.kt
+++ src/main/kotlin/org/physics/mamca/util/Mathematica.kt
@@ -16,11 +16,11 @@ object Mathematica {
     }
 
     fun findRoots(expr: String): List<Double> {
-        m.evaluate("NSolve[$expr, x, Reals]")
+        m.evaluate("x /. NSolve[$expr, x, Reals]")
         m.waitForAnswer()
         val exprRoots = m.expr
-        val roots: MutableList<Double> = mutableListOf()
-        (1..exprRoots.args().size).mapTo(roots) { exprRoots.part(intArrayOf(it, 1, 2)).asDouble() }
+        val roots: List<Double> = exprRoots.args().map {it.asDouble()}
+//        (1..exprRoots.args().size).mapTo(roots) { exprRoots.part(intArrayOf(it, 1, 2)).asDouble() }
         return roots
     }
 }
