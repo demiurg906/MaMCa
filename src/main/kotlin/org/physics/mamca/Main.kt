1c25791dcd18129ed219421ecda446e45bf5e1d4
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-11-30 11:15:39 +0300

    ENH: some log information about modelling added

--- src/main/kotlin/org/physics/mamca/Main.kt
+++ src/main/kotlin/org/physics/mamca/Main.kt
@@ -15,9 +15,19 @@ fun main(args: Array<String>) {
         settingsFile = args[0]
         outFolder = args[1]
     }
+    val startTime = System.currentTimeMillis()
+
     val settings = loadSettingsFromJson(settingsFile)
     val sample = Sample(settings)
     sample.dumpToJsonFile("$outFolder/sample.json")
     sample.optimizeEnergy()
     sample.saveState(outFolder = outFolder)
+
+    val endTime = System.currentTimeMillis()
+    val delimiter = "---------------------------------------------------"
+    println("\n$delimiter")
+    println("time of working is ${(endTime - startTime) / 1000.0} seconds")
+    println("sample size is ${settings.x}x${settings.y}x${settings.z} with ${settings.n} particles per ring")
+    println("total number of particles is ${settings.x * settings.y * settings.z * settings.n}")
+    println("$delimiter\n")
 }
