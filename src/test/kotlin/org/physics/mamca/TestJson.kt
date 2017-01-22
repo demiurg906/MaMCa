85d2985568d106b1421db95c66bffcac7141ec56
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-01-22 11:50:17 +0300

    TEST: change test for settings json mapper

--- src/test/kotlin/org/physics/mamca/TestJson.kt
+++ src/test/kotlin/org/physics/mamca/TestJson.kt
@@ -42,7 +42,22 @@ class TestJson {
 
     @Test
     fun testSettings() {
-        val settings = Settings()
+        val settings = Settings(
+                2, 3, 4, 5,
+                1.0, 2.0, 3.0,
+                4.0, 5.0, 6.0,
+                7.0, 8.0,
+                9.0, 10.0,
+                1, 11.0, 12.0,
+                1, 13.0, 14.0,
+                15.0, 16.0, 17.0,
+                18.0,
+                "name",
+                6, true, "json_path",
+                true, 8, 19.0,
+                "data_path"
+
+        )
         val filename = "src/test/resources/settings.json"
         createSettingsJson(filename, settings)
         val newSettings = loadSettingsFromJson(filename)
