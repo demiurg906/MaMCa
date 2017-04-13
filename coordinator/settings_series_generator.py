5fa66903f31cba8e6516410d24050b074a762375
Dmitry <dimon2x@gmail.com>
2017-04-13 14:17:51 +0300

    BUG: fix name of model

--- coordinator/settings_series_generator.py
+++ coordinator/settings_series_generator.py
@@ -15,7 +15,7 @@ def drange(start, stop, step):
 if __name__ == '__main__':
     setings_fname = './resources/settings.json'
     settings = Settings(setings_fname)
-    template = 'jex={:.2f}'
+    template = 'jex_{:.2f}'
     folder_template = './resources/settings/{}.json'
     for jex in drange(0.01, 0.1, 0.01):
         name = template.format(jex)
