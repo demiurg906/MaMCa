c3ff3b0f0d7b5ce91bfac09541db2e1b92f5df7f
Dmitry <dimon2x@gmail.com>
2017-05-04 17:05:59 +0300

    ENH: add exp range in settings generator script

--- coordinator/settings_series_generator.py
+++ coordinator/settings_series_generator.py
@@ -7,9 +7,16 @@ from mamca import *
 
 def drange(start, stop, step):
     r = start
-    while r < stop:
+    while r <= stop:
         yield float(r)
         r += step
+
+
+def mrange(start, step, n):
+    r = start
+    for _ in range(n):
+        yield float(r)
+        r *= step
 
 
 if __name__ == '__main__':
@@ -17,7 +24,7 @@ if __name__ == '__main__':
     settings = Settings(setings_fname)
     template = 'jex_{:.2f}'
     folder_template = './resources/settings/{}.json'
-    for jex in drange(0.01, 0.1, 0.01):
+    for jex in drange(0.0, 5.0, 0.5):
         name = template.format(jex)
         settings.jex = jex
         settings.name = name
